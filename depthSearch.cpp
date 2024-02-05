fe28f547ab4bdc3aba0deaa0369ede1ffe7d3390
Elias-bff <bartender.winery@gmail.com>
2024-02-05 18:33:59 -0500

    Update depthSearch.cpp

--- depthSearch.cpp
+++ depthSearch.cpp
@@ -1,53 +1,50 @@
 #include <iostream>
 #include <vector>
-#include <unordered_set>
 #include <stack>
+#include <unordered_set>
 
 class Graph {
-private:
-    int numVertices;
-    std::vector<std::vector<int>> adjacencyList;
+    private:
+        std::size_t numVertices;
+        std::vector<std::vector<std::size_t>> adjacencyList;
 
-public:
-    Graph(int vertices) : numVertices(vertices), adjacencyList(vertices) {}
+    public:
+        Graph(std::size_t vertices) : numVertices(vertices), adjacencyList(vertices) {}
 
-    void addEdge(int source, int destination) {
-        adjacencyList[source].push_back(destination);
-        // For an undirected graph, add the reverse edge as well
-        adjacencyList[destination].push_back(source);
-    }
+        void addEdge(std::size_t source, std::size_t destination) {
+            adjacencyList[source].push_back(destination);
+            adjacencyList[destination].push_back(source);
+        }
 
-    void DFS(int startNode) {
-        std::vector<bool> visited(numVertices, false);
-        std::stack<int> stack;
+        void DFS(std::size_t startNode) {
+            std::vector<bool> visited(numVertices, false);
+            std::stack<std::size_t> stack;
 
-        stack.push(startNode);
+            stack.push(startNode);
 
-        while (!stack.empty()) {
-            int currentVertex = stack.top();
-            stack.pop();
+            while (!stack.empty()) {
+                std::size_t currentVertex = stack.top();
 
-            if (!visited[currentVertex]) {
-                std::cout << currentVertex << " ";
-                visited[currentVertex] = true;
-            }
+                stack.pop();
 
-            // Reverse the order of neighbors to get the desired order
-            for (int i = adjacencyList[currentVertex].size() - 1; i >= 0; --i) {
-                int neighbor = adjacencyList[currentVertex][i];
-                if (!visited[neighbor]) {
-                    stack.push(neighbor);
+                if (!visited[currentVertex]) {
+                    std::cout << currentVertex << " ";
+                    visited[currentVertex] = true;
+
+                    for (auto it = adjacencyList[currentVertex].rbegin(); it != adjacencyList[currentVertex].rend(); ++it) {
+                        std::size_t neighbor = *it;
+                        if (!visited[neighbor]) {
+                            stack.push(neighbor);
+                        }
+                    }
                 }
             }
         }
-    }
 };
-
 
 int main() {
     Graph graph(4);
 
-    // Adding edges to the graph
     graph.addEdge(0, 1);
     graph.addEdge(0, 2);
     graph.addEdge(1, 2);
